382f8f0b74d8bed1455bca953f08644bd054afa0
songranshi <songran.shi@rwth-aachen.de>
2019-04-05 16:47:43 +0200

    Update Tile.java
    Added attributes and methods to Tile class

--- src/Tile.java
+++ src/Tile.java
@@ -4,18 +4,27 @@ public class Tile {
     Tile neighbours[];
     //0 empty Tile, 1-8 player Stones, 10 expension Stone
     byte stoneColour;
-    TileProp tileProp;
+    char tileProp;
 
     final int x;
     final int y;
 
-    public Tile(byte sC, TileProp tP, int xPos, int yPos){
+    public Tile(byte sC, char tP, int xPos, int yPos){
         stoneColour = sC;
         tileProp = tP;
 
         x = xPos;
         y = yPos;
     }
+
+    public void setStone(byte stoneColour) {
+        this.stoneColour = stoneColour;
+    }
+
+    public void setTileProp(char tP) {
+        this.tileProp = tP;
+    }
+
 
     public void setNeighbours(Tile[] neighbours) {
         this.neighbours = neighbours;
@@ -28,12 +37,13 @@ public class Tile {
     public int getY() {
         return y;
     }
+
+    public Tile getNeighbour(int direction) { return neighbours[direction]; }
+
+    public byte getStoneColour() { return stoneColour; }
+
+    public char getTileProp() { return tileProp; }
+
 }
 
-enum TileProp{
-    CHOICE,
-    NONE,
-    INVERSION,
-    BONUS
-}
 
