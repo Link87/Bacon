ef89ffdc0fb6283c5551475bd5f2c040824a59cc
Marvin Gazibaric <marvin.gazibaric@rwth-aachen.de>
2019-04-19 16:52:41 +0200

    Add dedicated tests for regular and override stones

---
+++ src/test/bacon/OverrideMoveTest.java
@@ -0,0 +1,33 @@
+package bacon;
+
+import org.junit.Test;
+
+import static bacon.Tile.Property.DEFAULT;
+import static org.junit.Assert.*;
+
+public class OverrideMoveTest {
+
+    @Test
+    public void isLegal() {
+        Game.getGame().readMap(bacon.Maps.STARFISH);
+        var map = Game.getGame().getMap();
+
+        map.getTileAt(9, 1).setOwner(Game.getGame().getPlayerFromNumber(6));
+
+        assertTrue(Move.createNewMove(0, map, Game.getGame().getPlayerFromNumber(8),9, 1, 0).isLegal());
+        assertFalse(Move.createNewMove(0, map, Game.getGame().getPlayerFromNumber(8),10, 1, 0).isLegal());
+
+    }
+
+    @Test
+    public void doMove() {
+        Game.getGame().readMap(bacon.Maps.STARFISH);
+        var map = Game.getGame().getMap();
+
+        map.getTileAt(11, 3).setProperty(Tile.Property.EXPANSION);
+        Move.createNewMove(0, map, Game.getGame().getPlayerFromNumber(8),11, 3, 0).doMove();
+
+        assertEquals(DEFAULT, map.getTileAt(11, 3).getProperty());
+        assertEquals(5, Game.getGame().getPlayerFromNumber(8).getOverrideStoneCount());
+    }
+}
---
+++ src/test/bacon/RegularMoveTest.java
@@ -0,0 +1,43 @@
+package bacon;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class RegularMoveTest {
+
+    @Test
+    public void doMove() {
+        Game.getGame().readMap(bacon.Maps.STARFISH);
+        var map = Game.getGame().getMap();
+
+        // Test inversion tiles
+        var tiles = new ArrayList<Tile>();
+        var owners = new ArrayList<Player>();
+
+        for (int x = 0; x < 33; x++) {
+            for (int y = 0; y < 33; y++) {
+                if (map.getTileAt(x, y).getOwner() != null) {
+                    tiles.add(map.getTileAt(x, y));
+                    if (x == 9 && (y == 1 || y == 2))
+                        owners.add(Game.getGame().getPlayerFromNumber(8));
+                    else owners.add(map.getTileAt(x, y).getOwner());
+                }
+            }
+        }
+
+        map.getTileAt(9, 1).setProperty(Tile.Property.INVERSION);
+        Move.createNewMove(0, map, Game.getGame().getPlayerFromNumber(8), 9, 1, 0).doMove();
+
+        for (int i = 0; i < tiles.size(); i++) {
+            int num = (owners.get(i).getPlayerNumber() + 1) % (Game.getGame().getTotalPlayerCount() + 1);
+            if (num == 0) num = 1;
+            assertTrue("Error at tile " + tiles.get(i).x + ", " + tiles.get(i).y + " num=" + num,
+                    num >= 1 && num <= Game.getGame().getTotalPlayerCount());
+            assertEquals("Error at tile " + tiles.get(i).x + ", " + tiles.get(i).y,
+                    num, tiles.get(i).getOwner().getPlayerNumber());
+        }
+    }
+}
