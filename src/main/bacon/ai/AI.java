a7e203a9c6d8003f778d42b2dd93e0cb4ccf0658
spacecheck <45857014+spacecheck@users.noreply.github.com>
2019-04-28 16:53:11 +0200

    simple ai (no Bomb heuristic)

--- src/main/bacon/ai/AI.java
+++ src/main/bacon/ai/AI.java
@@ -1,18 +1,23 @@
 package bacon.ai;
 
-import bacon.Game;
-import bacon.GameState;
-import bacon.Map;
-import bacon.Tile;
+import bacon.*;
 import bacon.move.BonusRequest;
 import bacon.move.Move;
 import bacon.move.MoveFactory;
+import bacon.ai.heuristics.*;
+
+import java.util.Set;
 
 public class AI {
 
     private static final AI INSTANCE = new AI();
+    public double stbltyScaler = 1;
+    public double clstrngScaler = 1;
+    public double mobltyScaler = 1;
+    public double bonusScalar = 1;
 
-    private AI() {}
+    private AI() {
+    }
 
     public static AI getAI() {
         return INSTANCE;
@@ -21,25 +26,76 @@ public class AI {
     /**
      * Request a move from the ai.
      *
-     * @param timeout the time the ai has for its computation
-     * @param depth   the maximum search depth the ai is allowed to do
+     * @param timeout          the time the ai has for its computation
+     * @param depth            the maximum search depth the ai is allowed to do
      * @param currentGameState current  Game State
      * @return the next move
      */
     public Move requestMove(int timeout, int depth, GameState currentGameState) {
-        Map map = currentGameState.getMap();
-        for (int x = 0; x < map.width; x++) {
-            for (int y = 0; y < map.height; y++) {
-                BonusRequest br = null;
-                if(map.getTileAt(x,y).getProperty()== Tile.Property.CHOICE)  br = BonusRequest.fromValue(1,currentGameState);
-                if(map.getTileAt(x,y).getProperty()== Tile.Property.BONUS) br = BonusRequest.fromValue(21,currentGameState);
-                var move = MoveFactory.createMove(Game.getGame().getCurrentState(),
-                        Game.getGame().getCurrentState().getMe(), x, y, br);
-                if (move.isLegal())
-                    return move;
+        Set<Tile> moveTiles = LegalMoves.getLegalMoveTiles(currentGameState, currentGameState.getMe().getPlayerNumber(), MoveType.REGULAR);
+        Move curBestMove = null;
+        if (moveTiles.isEmpty())
+            moveTiles = LegalMoves.getLegalMoveTiles(currentGameState, currentGameState.getMe().getPlayerNumber(), MoveType.OVERRIDE);
+        if (currentGameState.getGamePhase() == GamePhase.PHASE_ONE) {
+            double evalValue;
+            double curBestVal = Double.MIN_VALUE;
+            for (Tile tile : moveTiles) {
+                if (tile.getProperty() == Tile.Property.CHOICE) {
+                    for (int numbr = 1; numbr <= currentGameState.getTotalPlayerCount(); numbr++) {
+                        GameState evalState = currentGameState.getDeepCopy();
+                        Move moveToEval = MoveFactory.createMove(evalState, evalState.getMe(), tile.x, tile.y, BonusRequest.fromValue(numbr, evalState));
+                        moveToEval.doMove();
+                        int meNumbr = evalState.getMe().getPlayerNumber();
+                        evalValue = stbltyScaler * StabilityHeuristic.stability(evalState, meNumbr)
+                                + clstrngScaler * Heuristics.clustering(evalState, meNumbr)
+                                + mobltyScaler * Heuristics.mobility(evalState, meNumbr);
+                        if(evalValue > curBestVal){
+                            curBestVal = evalValue;
+                            curBestMove = moveToEval;
+                        }
+                    }
+                } else if (tile.getProperty() == Tile.Property.BONUS){
+                    GameState evalState = currentGameState.getDeepCopy();
+                    Move moveToEval = MoveFactory.createMove(evalState, evalState.getMe(), tile.x, tile.y, new BonusRequest(BonusRequest.Type.BOMB_BONUS));
+                    moveToEval.doMove();
+                    int meNumbr = evalState.getMe().getPlayerNumber();
+                    evalValue = stbltyScaler * StabilityHeuristic.stability(evalState, meNumbr)
+                            + clstrngScaler * Heuristics.clustering(evalState, meNumbr)
+                            + mobltyScaler * Heuristics.mobility(evalState, meNumbr)
+                            + bonusScalar *  Heuristics.bonusBomb(evalState,meNumbr);
+                    if(evalValue > curBestVal){
+                        curBestVal = evalValue;
+                        curBestMove = moveToEval;
+                    }
+
+                    evalState = currentGameState.getDeepCopy();
+                    moveToEval = MoveFactory.createMove(evalState, evalState.getMe(), tile.x, tile.y, new BonusRequest(BonusRequest.Type.OVERRIDE_BONUS));
+                    moveToEval.doMove();
+                    meNumbr = evalState.getMe().getPlayerNumber();
+                    evalValue = stbltyScaler * StabilityHeuristic.stability(evalState, meNumbr)
+                            + clstrngScaler * Heuristics.clustering(evalState, meNumbr)
+                            + mobltyScaler * Heuristics.mobility(evalState, meNumbr)
+                            + bonusScalar * Heuristics.bonusOverride(evalState,meNumbr);
+                    if(evalValue > curBestVal){
+                        curBestVal = evalValue;
+                        curBestMove = moveToEval;
+                    }
+                } else {
+                    GameState evalState = currentGameState.getDeepCopy();
+                    Move moveToEval = MoveFactory.createMove(evalState, evalState.getMe(), tile.x, tile.y);
+                    moveToEval.doMove();
+                    int meNumbr = evalState.getMe().getPlayerNumber();
+                    evalValue = stbltyScaler * StabilityHeuristic.stability(evalState, meNumbr)
+                            + clstrngScaler * Heuristics.clustering(evalState, meNumbr)
+                            + mobltyScaler * Heuristics.mobility(evalState, meNumbr);
+                    if(evalValue > curBestVal){
+                        curBestVal = evalValue;
+                        curBestMove = moveToEval;
+                    }
+                }
+
             }
         }
-        return null;
+        return curBestMove;
     }
-
 }
