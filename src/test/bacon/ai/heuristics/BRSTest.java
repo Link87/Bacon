e6ad5e8074edb905d6d24e2f263b194956e74d05
Marlene Ida Damm <marlene.damm@rwth-aachen.de>
2019-05-12 14:18:29 +0200

    BRS Test for Capturing Bonus Tiles
    not finished yet

--- src/test/bacon/ai/heuristics/BRSTest.java
+++ src/test/bacon/ai/heuristics/BRSTest.java
@@ -6,12 +6,15 @@ import bacon.Maps;
 import bacon.Tile;
 import bacon.ai.BRSNode;
 import bacon.Player;
+import bacon.move.BuildMove;
 import bacon.move.Move;
+import bacon.move.RegularMove;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -19,11 +22,11 @@ public class BRSTest {
 
     @Test
     public void legal() {
-        Game.getGame().readMap(Maps.EXAMPLE_CERTAIN);
+        Game.getGame().readMap(Maps.EXAMPLE_BRS);
         Player me = Game.getGame().getCurrentState().getPlayerFromNumber(1);
         Game.getGame().getCurrentState().setMe(me);
 
-        BRSNode root = new BRSNode(0, 15, 2, true, null);
+        BRSNode root = new BRSNode(0, 5, 1, true, null);
         root.evaluateNode();
         Move bestMove = root.getBestMove();
 
@@ -33,6 +36,44 @@ public class BRSTest {
         System.out.println(Game.getGame().getCurrentState().getMap().toString());
 
         assertTrue("BRS returns illegal move", bestMove.isLegal());
+
+        //Does BRS try to capture bonus tiles ?
+        Game.getGame().readMap(Maps.EXAMPLE_BRS2);
+        me = Game.getGame().getCurrentState().getPlayerFromNumber(1);
+        Game.getGame().getCurrentState().setMe(me);
+
+        for(int i = 0; i < 20; i++) {
+            root = new BRSNode(0, 3, 5, true, null);
+            root.evaluateNode();
+            bestMove = root.getBestMove();
+
+            assertTrue("BRS returns no move", bestMove != null);
+            System.out.println("Best Move: " + "(" + bestMove.getX() + "," + bestMove.getY() + ")");
+
+
+            assertTrue("BRS returns illegal move", bestMove.isLegal());
+
+            bestMove.doMove();
+            System.out.println(Game.getGame().getCurrentState().getMap().toString());
+
+            for(int j = 2; j < 4; j++) {
+                Set<RegularMove> move = LegalMoves.getLegalRegularMoves(Game.getGame().getCurrentState(), j);
+                int size = move.size();
+                int index = (int) (Math.random() * (size));
+
+                Move doMove = null;
+                for (Move m : move) {
+                    if (index == 0) {
+                        doMove = m;
+                        break;
+                    }
+                    index--;
+                }
+
+                doMove.doMove();
+            }
+        }
+        System.out.println("BombCount: " + me.getBombCount() + "OverrideCount: " + me.getOverrideStoneCount());
     }
 
 }
